20140d84704657c3d5dde308ee8b045d3e32faf7
Yevhen Shumeiko <shymeiko.zenya@gmail.com>
2024-07-14 13:43:37 +0300

    initialCommit

---
+++ src/main/java/org/example/eventspotlightback/EventSpotlightBackApplication.java
@@ -0,0 +1,13 @@
+package org.example.eventspotlightback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventSpotlightBackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EventSpotlightBackApplication.class, args);
+    }
+
+}
